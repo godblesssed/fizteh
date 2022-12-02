049a7b855a4703aefd2f79735e75007a758349cd
godblesssed <119662355+godblesssed@users.noreply.github.com>
2022-12-02 14:18:23 +0500

    Create fizteh.user.js

---
+++ fizteh.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         fizteh ololo
+// @version      0.1
+// @description  vzlom fizteha!!!!!!
+// @author       godblesssed
+// @match        *://olymp-online.mipt.ru/olympic/solve/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelectorAll(".task_statement span").forEach(e => e.remove()) // omg mega hack elon musk v shoke
+})();
